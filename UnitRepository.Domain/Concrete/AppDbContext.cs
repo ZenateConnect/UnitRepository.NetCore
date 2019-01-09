efeca0681ded4fc319e7813fd10fe7f4d9192749
SixnaskUnz <sixnaskunz@outlook.com>
2019-01-09 09:50:45 +0700

    Add AppDbContext

---
+++ UnitRepository.Domain/Concrete/AppDbContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnitRepository.Model.Core;
+
+namespace UnitRepository.Domain.Concrete
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+
+        }
+
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        DbSet<SampleClass> SampleClasses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // Windows Authentication.
+                optionsBuilder.UseSqlServer("Server=.\\INITIALIZE;Database=TestDb;Integrated Security=True;");
+
+                // SQL Authentication.
+                //optionsBuilder.UseSqlServer("Server=.\\INITIALIZE;Database=TruckDb;User Id=xxx;Password=xxx");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
